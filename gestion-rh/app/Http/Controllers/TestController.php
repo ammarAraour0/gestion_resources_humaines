79ca69aac21d329a0dcee43fae5455aa598a6e42
Ammar0Araour <ammar.araour06@gmail.com>
2025-01-05 23:06:41 +0100

    Initialisation du projet

---
+++ gestion-rh/app/Http/Controllers/TestController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+        return "Atelier 1 terminé avec succès !";
+    }
+}
